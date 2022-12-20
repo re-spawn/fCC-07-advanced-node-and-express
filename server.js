ad7468f1127eda3c9bb2858dd33b79c637efbee0
re-spawn <rob@robanthony.ca>
2022-12-20 14:32:00 -0330

    feat: passport setup

--- server.js
+++ server.js
@@ -11,11 +11,109 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+const session = require('express-session');
+const passport = require('passport');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true
+    });
+  });
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
+      return done(null, user);
+    });
+  }));
+  app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/profile');
+  });
+  app.route('/register').post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) { next(err); }
+      else if (user) { res.redirect('/'); }
+      else {
+        const hash = bcrypt.hashSync(req.body.password, 12);
+        myDataBase.insertOne({
+	  username: req.body.username,
+	  password: hash
+	}, (err, doc) => {
+	  if (err) { res.redirect('/'); }
+	  else {
+	    next(null, doc.ops[0]);
+	  }
+	});
+      }
+    });
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res) => {
+        res.redirect('/profile');
+      }
+  );
+  app.get('/profile',
+    ensureAuthenticated,
+    (req, res) => {
+      res.render('profile', {
+        username: req.user.username
+      });
+  });
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: e,
+      message: 'Unable to connect to database'
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
